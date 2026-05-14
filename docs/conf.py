b986f31e6661b59b8ccd60f1d2c89eaa5a474bf7
Rohan Varadaraj <rohanva@gmail.com>
2026-05-14 15:32:18 +0100

    selected n when running sphinx thingy

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'readme'
-copyright = '2026, Rohan'
-author = 'Rohan'
-release = '0.1'
+project = 'rse'
+copyright = '2026, rohan'
+author = 'rohan'
+release = '1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -17,7 +17,7 @@ release = '0.1'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
